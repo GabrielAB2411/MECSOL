9d61a150c0488b2724f32299b489c78e6f837215
Gabriel <gabrielab2411@gmail.com>
2024-05-15 19:19:23 -0300

    [BUGFIX] - Fixed URL redirection

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $('#lblCurrentYear').text(`© ${new Date().getFullYear().toString()}`);
     
     $('#btnFundamentals').click(function () {
-        window.location.replace('/src/pages/contents/fundamentals.html');
+        window.location.replace('src/pages/contents/fundamentals.html'); // MECSOL/
     });
 
     $('#btnTractionCompression').click(function () {
-        window.location.replace('/src/pages/contents/tractionCompression.html');
+        window.location.replace('src/pages/contents/tractionCompression.html'); // MECSOL/
     });
 
     $('#btnShear').click(function () {
-        window.location.replace('/src/pages/contents/shear.html');
+        window.location.replace('src/pages/contents/shear.html'); // MECSOL/
     });
 });
