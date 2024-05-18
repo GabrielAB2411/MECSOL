c71bd98bfc1a5c6e3bfb19abd150b95fd9d176c4
Gustavo Braz <brazgustav@gmail.com>
2024-05-18 16:17:36 -0300

    [FEAT] Added random image display to exercise accordion opening

--- src/js/tractionCompression.js
+++ src/js/tractionCompression.js
@@ -1,11 +1,37 @@
 var collapseFour = '#collapseFour';
+var collapseFourAccordionBody = '#collapseFour .accordion-body';
+
+var imagePaths = [
+    { name: 'Compression Circle', path: '../../assets/compressionCircle.png' },
+    { name: 'Compression Square', path: '../../assets/compressionSquare.png' },
+    { name: 'Traction Circle', path: '../../assets/tractionCircle.png' },
+    { name: 'Traction Square', path: '../../assets/tractionSquare.png' }
+];
 
 $(document).ready(function(){
     $(collapseFour).on('shown.bs.collapse', function () {
         handleExerciseAccordionOpen();
     });
+
+    $(collapseFour).on('hide.bs.collapse', function () {
+        handleExerciseAccordionClose();
+    });
 });
 
 function handleExerciseAccordionOpen() {
+    var randomIndex = Math.floor(Math.random() * imagePaths.length);
+    var randomImagePath = imagePaths[randomIndex].path;
+    var img = document.createElement('img');
+    img.src = randomImagePath;
+    img.alt = 'Random Image';
     
+    var accordionBody = document.querySelector(collapseFourAccordionBody);
+    
+    accordionBody.innerHTML = '';
+    accordionBody.appendChild(img);
+}
+
+function handleExerciseAccordionClose() {
+    var accordionBody = document.querySelector(collapseFourAccordionBody);
+    accordionBody.innerHTML = '';
 }
